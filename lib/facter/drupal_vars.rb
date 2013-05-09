02bcfc339e7bd68e4c016eeb9be4902566f77142
root <root@yarrow.puppetlabs.vm>
2013-05-09 14:51:16 +0000

    First attempt to add drupal_X
    Doesn't handle arrays

---
+++ lib/facter/drupal_vars.rb
@@ -0,0 +1,7 @@
+IO.popen("drush vget").readlines.each do |line|
+  next if line =~ /(?:^[\s\(\)]|^\s*$|:\s*Array\s*$)/
+  match = line.match(/(\S+):\s*(\S.*)/) or next
+  Facter.add("drupal_#{match[1]}") do
+    setcode { match[2].gsub(/^"|"$/, "") }
+  end
+end
