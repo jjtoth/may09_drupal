9d945fccd963125a88044d964c861fdc65817667
Jim Toth <jjtoth@vcu.edu>
2013-05-09 14:45:38 -0400

    Add drupal_var type

---
+++ lib/puppet/type/drupal_var.rb
@@ -0,0 +1,13 @@
+Puppet::Type.newtype(:drupal_var) do
+  desc "Set Drupal variables"
+
+  newparam(:var_name) do
+    desc "Name of Drupal Variable"
+    isnamevar
+  end
+
+  newproperty(:value) do
+    desc "Current value of Drupal variable"
+  end
+
+end
